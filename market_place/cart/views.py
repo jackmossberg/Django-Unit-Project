65c32b1b042ef0f61039ca251b2fc563e39ce19a
mrdiabeatis <jackmossberg25@gmail.com>
2025-11-25 23:44:41 -0600

    categories / some general cleanup

--- market_place/cart/views.py
+++ market_place/cart/views.py
@@ -8,6 +8,7 @@ from django.conf import settings
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
+@login_required
 def checkout_cart(request):
     
     cart = Cart.objects.get(user=request.user)
@@ -49,11 +50,13 @@ def checkout_cart(request):
 
     return redirect(checkout_session.url)
 
+@login_required
 def checkout_success(request):
     CartItem.objects.filter(cart__user=request.user).delete()
 
     return redirect('Cart:Home')
 
+@login_required
 def add_cart(request, pk):
     added_item = get_object_or_404(Item,pk=pk)
     cart, created = Cart.objects.get_or_create(user=request.user)
@@ -65,6 +68,7 @@ def add_cart(request, pk):
         cart_item.save()
     return redirect('app:home')
 
+@login_required
 def delete_cart(request, pk):
     cart = get_object_or_404(Cart, user=request.user)
 
@@ -75,6 +79,7 @@ def delete_cart(request, pk):
     else:
         cart_item.delete()
     return redirect("Cart:Home")
+
 @login_required
 def cart(request):
      cart, created = Cart.objects.get_or_create(user=request.user)
